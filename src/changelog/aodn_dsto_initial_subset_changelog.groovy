1dbe1f7e2047e87df20c8ce46a19983e4fca362b
Jon Burgess <jkburges@gmail.com>
2015-02-23 10:27:26 +1100

    Example 'initial subset' migration.
    This is what will be run by *some* automated process to create a subsetted backup of production.
    This will then be the basis for what's restored on the development VM.

---
+++ src/changelog/aodn_dsto_initial_subset_changelog.groovy
@@ -0,0 +1,9 @@
+databaseChangeLog() {
+    changeSet(author: 'jkburges', id: '1424644927') {
+
+        // Example migration to subset the full production data.
+        // delete(tableName: 'measurements') {
+        //     where("ST_Disjoint(geom, 'POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))'::geography::geometry)")
+        // }
+    }
+}
