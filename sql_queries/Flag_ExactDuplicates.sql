e24f81a323a9148b210a6f4145b1a2da665b7765
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-03-16 15:57:16 +1100

    Amend SQL query to flag exact duplicates i.e. based on time, longitude and latitude

--- sql_queries/Flag_ExactDuplicates.sql
+++ sql_queries/Flag_ExactDuplicates.sql
@@ -1,15 +1,14 @@
 ﻿SET SEARCH_PATH = marvl3, public;
 
--- Identify XBT duplicates
-UPDATE spatial_subset SET duplicate = TRUE
-FROM (WITH a AS (SELECT origin_id, 
-	"LONGITUDE", 
+-- Identify profile duplicates
+UPDATE spatial_subset SET duplicate = TRUE, duplicate_id = dupl_id
+FROM (SELECT "LONGITUDE", 
 	"LATITUDE", 
-	"TIME" 
+	"TIME",
+	nextval('duplicate_seq') AS dupl_id
   FROM spatial_subset 
-	GROUP BY origin_id, "LONGITUDE", "LATITUDE", "TIME"), 
-  b AS (SELECT row_number() over (partition by "LONGITUDE", "LATITUDE", "TIME" order by origin_id) as rnum,* 
-  FROM a) 
-  SELECT * 
-  FROM b)aggregated
-WHERE aggregated.origin_id = spatial_subset.origin_id AND rnum > 1
+  LEFT JOIN source ON source.source_id = spatial_subset.source_id
+	WHERE source."SUBFACILITY" IN ('XBT','SATTAG', 'APB')
+	GROUP BY "LONGITUDE", "LATITUDE", "TIME"
+	HAVING count(DISTINCT origin_id) > 1)aggregated
+WHERE aggregated."LONGITUDE" = spatial_subset."LONGITUDE" AND aggregated."LATITUDE" = spatial_subset."LATITUDE" AND aggregated."TIME" = spatial_subset."TIME";
