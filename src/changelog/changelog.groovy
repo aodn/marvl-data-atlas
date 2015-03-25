8e884381ffba377acf2b5b0b24c117cf2df838ae
Jon Burgess <jkburges@gmail.com>
2015-03-25 11:42:57 +1100

    Fixed typo, which was causing migrations to fail

--- src/changelog/changelog.groovy
+++ src/changelog/changelog.groovy
@@ -1,4 +1,4 @@
 databaseChangeLog() {
-    include(file: 'subset_changelog'.groovy', relativeToChangeLogFile: true)
+    include(file: 'subset_changelog.groovy', relativeToChangeLogFile: true)
     include(file: 'aodn_dsto_changelog.groovy', relativeToChangelogFile: true)
 }
