cf66769910ca18a72164ffb5602d4ee1178185cb
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-04-15 09:29:21 +1000

    Deleted IMOS SOOP XBT and CSIRO XBT

--- sql_queries/Populate_SpatialSubsetTable.sql
+++ sql_queries/Populate_SpatialSubsetTable.sql
@@ -1,58 +1,5 @@
 ﻿SET SEARCH_PATH = marvl3, public;
 
--- SOOP_XBT_DM
-INSERT INTO spatial_subset(
-	WITH m AS (SELECT profile_id, source_id FROM soop_xbt_dm.soop_xbt_dm_profile_map, "500m_isobath",source WHERE ST_CONTAINS("500m_isobath".geom, soop_xbt_dm_profile_map.geom) AND source_id = 18)
-  SELECT source_id,
-	m.profile_id,
-	"LONGITUDE",
-	"LONGITUDE_quality_control",
-	"LATITUDE",
-	"LATITUDE_quality_control",
-	"TIME",
-	"TIME_quality_control",
-	"DEPTH",
-	"DEPTH_quality_control",
-	"TEMP",
-	"TEMP_quality_control",
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	geom
-  FROM m
-  LEFT JOIN soop_xbt_dm.soop_xbt_dm_profile_data d ON m.profile_id = d.profile_id
-    WHERE "TIME" >= '1995-01-01'
-	);
-
--- CSIRO XBT
-INSERT INTO spatial_subset(
-  SELECT source_id,
-	"SURVEY_ID",
-	"LONGITUDE",
-	NULL,
-	"LATITUDE",
-	NULL,
-	"TIME" AT TIME ZONE 'UTC',
-	NULL,
-	"DEPTH",
-	NULL,
-	"TEMPERATURE",
-	"TEMPERATURE_QC",
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	m.geom
-  FROM aodn_csiro_cmar.aodn_csiro_cmar_xbt_data m, "500m_isobath", source
-  WHERE ST_CONTAINS("500m_isobath".geom, m.geom) AND
-  	source_id = 34
-	);
-
 -- WODB XBT
 INSERT INTO spatial_subset(
 	WITH m AS (SELECT "CAST_ID", "LONGITUDE", "LATITUDE", "TIME", source_id, xbt_deployments.geom FROM wodb.xbt_deployments, "500m_isobath",source WHERE ST_CONTAINS("500m_isobath".geom, xbt_deployments.geom) AND source_id = 45)
@@ -77,6 +24,7 @@ INSERT INTO spatial_subset(
 	geom
   FROM m
   LEFT JOIN wodb.xbt_measurements d ON m."CAST_ID" = d.cast_id
+	WHERE "TIME" >= '1995-01-01' AND "TIME" < '2015-01-01'
 	);
 
 	
@@ -102,34 +50,7 @@ INSERT INTO spatial_subset(
 	NULL,
 	m.geom
   FROM aatams_sattag_dm.aatams_sattag_dm_profile_data m, "500m_isobath", source
-  WHERE ST_CONTAINS("500m_isobath".geom, m.geom) AND
-  	source_id = 1
-	);
-
--- WODB APB
-INSERT INTO spatial_subset(
-	WITH m AS (SELECT "CAST_ID", "LONGITUDE", "LATITUDE", "TIME", source_id, apb_deployments.geom FROM wodb.apb_deployments, "500m_isobath",source WHERE ST_CONTAINS("500m_isobath".geom, apb_deployments.geom) AND source_id = 37)
-  SELECT source_id,
-	m."CAST_ID",
-	"LONGITUDE",
-	NULL,
-	"LATITUDE",
-	NULL,
-	"TIME",
-	NULL,
-	depth,
-	NULL,
-	temperature,
-	NULL,
-	salinity,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	geom
-  FROM m
-  LEFT JOIN wodb.apb_measurements d ON m."CAST_ID" = d.cast_id
+  WHERE ST_CONTAINS("500m_isobath".geom, m.geom) AND source_id = 1 AND timestamp >= '1995-01-01' AND timestamp < '2015-01-01'
 	);
 
 -- AODN RAN CTD
