498cdb1d58053532493f8507df5730827b47fee2
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-04-15 11:01:44 +1000

    Comment out where clause in exactduplicate query

--- sql_queries/Flag_ExactDuplicates.sql
+++ sql_queries/Flag_ExactDuplicates.sql
@@ -8,7 +8,7 @@ FROM (SELECT "LONGITUDE",
 	nextval('duplicate_seq') AS dupl_id
   FROM spatial_subset 
   LEFT JOIN source ON source.source_id = spatial_subset.source_id
-	WHERE source."SUBFACILITY" IN ('XBT','SATTAG', 'APB')
+-- 	WHERE source."SUBFACILITY" IN ('XBT','SATTAG', 'APB')
 	GROUP BY "LONGITUDE", "LATITUDE", "TIME"
 	HAVING count(DISTINCT origin_id) > 1)aggregated
 WHERE aggregated."LONGITUDE" = spatial_subset."LONGITUDE" AND aggregated."LATITUDE" = spatial_subset."LATITUDE" AND aggregated."TIME" = spatial_subset."TIME";
