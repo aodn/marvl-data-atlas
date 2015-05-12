3968c5ead69e0a8b174b5405561be00b5e41c0d3
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-05-12 16:45:48 +1000

    Changed max to min when calculating bathymetry

--- sql_queries/Populate_BathyAtlasTable.sql
+++ sql_queries/Populate_BathyAtlasTable.sql
@@ -10,6 +10,6 @@ INSERT INTO bathy_atlas (
 select
 (width_bucket(w.longitude, 110.875, 155.125, 176)-1)*0.25+111,
 (width_bucket(w.latitude, -2.875, -45.125, 168)-1)*-0.25-3,
-max(w.depth)
+min(w.depth)
 from legacy_bathy.world_depth w
 GROUP BY width_bucket(w.longitude, 110.875, 155.125, 176), width_bucket(w.latitude, -2.875, -45.125, 168);
