c67c0ac69ceed733b448d7397d05c0a18eb56ab8
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-05-13 09:50:32 +1000

    Add query to delete rows in data_atlas that are below the seafloor

---
+++ sql_queries/DeleteFrom_DataAtlasTable.sql
@@ -0,0 +1,12 @@
+﻿SET SEARCH_PATH = marvl3, public;
+
+-- delete_bins_from_data_atlas_based_on_bathy
+\echo 'delete_bins_from_data_atlas_based_on_bathy'
+DELETE FROM data_atlas s
+WHERE (s."LONGITUDE", s."LATITUDE", s."TIME", s."DEPTH") IN (
+SELECT s."LONGITUDE", s."LATITUDE", s."TIME", s."DEPTH" 
+FROM data_atlas s
+INNER JOIN bathy_atlas b
+ON s."LONGITUDE" = b."LONGITUDE"
+AND s."LATITUDE" = b."LATITUDE"
+WHERE s."DEPTH" * (-1) < b."DEPTH");
