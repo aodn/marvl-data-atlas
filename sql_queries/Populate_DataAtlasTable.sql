ea8c09284832341700c0b0b3f1ad01bf762d4803
Xavier Hoenner <xavier.hoenner@utas.edu.au>
2015-05-15 09:34:26 +1000

    add in comment that the added global range check comes from ARGO QC practices

--- sql_queries/Populate_DataAtlasTable.sql
+++ sql_queries/Populate_DataAtlasTable.sql
@@ -40,16 +40,16 @@ date_trunc('month', s."TIME" AT TIME ZONE 'UTC') + interval '1 mons' AS "TIME_bo
 (width_bucket(CASE WHEN "DEPTH_QC" IN ('0', '1', '2') THEN s."DEPTH" ELSE s."NOMINAL_DEPTH" END, -5, 505, 51)-1)*10 AS "DEPTH", -- we consider DEPTH buckets of size 10 with first value centred on 0 : [-5;5[, [5;15[, etc... If field DEPTH is NULL, NOMINAL_DEPTH is considered.
 (width_bucket(CASE WHEN "DEPTH_QC" IN ('0', '1', '2') THEN s."DEPTH" ELSE s."NOMINAL_DEPTH" END, -5, 505, 51)-1)*10-5 AS "DEPTH_bound_min",
 (width_bucket(CASE WHEN "DEPTH_QC" IN ('0', '1', '2') THEN s."DEPTH" ELSE s."NOMINAL_DEPTH" END, -5, 505, 51)-1)*10+5 AS "DEPTH_bound_max",
-count(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_n", -- measurements with QC flags no good are not considered
-min(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_min",
-max(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_max",
-avg(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_mean",
-stddev(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_stddev",
-count(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_n", -- checking for TEMP_QC is part of the salinity QC test (if TEMP is not good then PSAL must be not good)
-min(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_min",
-max(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_max",
-avg(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_mean",
-stddev(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_stddev",
+count(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_n", -- measurements with QC flags no good are not considered, global range QC test (ARGO thresholds)
+min(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_min", -- global range QC test (ARGO thresholds)
+max(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_max", -- global range QC test (ARGO thresholds)
+avg(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_mean", -- global range QC test (ARGO thresholds)
+stddev(CASE WHEN s."TEMP_QC" IN ('0', '1', '2') AND s."TEMP" BETWEEN -2.5 AND 40 THEN s."TEMP" ELSE NULL END) AS "TEMP_stddev", -- global range QC test (ARGO thresholds)
+count(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_n", -- checking for TEMP_QC is part of the salinity QC test (if TEMP is not good then PSAL must be not good), global range QC test (ARGO thresholds)
+min(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_min", -- global range QC test (ARGO thresholds)
+max(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_max", -- global range QC test (ARGO thresholds)
+avg(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_mean", -- global range QC test (ARGO thresholds)
+stddev(CASE WHEN (s."PSAL_QC" IN ('0', '1', '2') AND s."PSAL" BETWEEN 2 AND 41 AND s."TEMP_QC" IN ('0', '1', '2')) THEN s."PSAL" ELSE NULL END) AS "PSAL_stddev", -- global range QC test (ARGO thresholds)
 ST_GeometryFromText(COALESCE('POLYGON(('||(width_bucket(s."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+111.125||' '||(width_bucket(s."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-2.875||', '||(width_bucket(s."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+111.125||' '||(width_bucket(s."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-3.125||', '||(width_bucket(s."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+110.875||' '||(width_bucket(s."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-3.125||', '||(width_bucket(s."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+110.875||' '||(width_bucket(s."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-2.875||', '||(width_bucket(s."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+111.125||' '||(width_bucket(s."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-2.875||'))'), '4326')
 FROM marvl3.spatial_subset s
 WHERE "LONGITUDE_QC" IN ('0', '1', '2') -- measurements with time and space location QC flags no good are not considered
