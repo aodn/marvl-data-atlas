aeb49e2b7748975841e558bdeeaf3ad084b133fe
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-05-15 14:21:05 +1000

    AUV are now 1min averaged and sub-sampled every 5min

--- sql_queries/Populate_SpatialSubsetTable_auv.sql
+++ sql_queries/Populate_SpatialSubsetTable_auv.sql
@@ -22,22 +22,24 @@ geom
 SELECT 
 s.source_id,
 d.file_id,
-d."LONGITUDE",
+avg(d."LONGITUDE"),
 '1',
-d."LATITUDE",
+avg(d."LATITUDE"),
 '1',
-d."TIME" AT TIME ZONE 'UTC',
+date_trunc('minute', d."TIME" AT TIME ZONE 'UTC'),
 '1',
-d."DEPTH",
+avg(d."DEPTH"),
 '0',
-d."TEMP",
+avg(d."TEMP"),
 '0',
-d."PSAL",
+avg(d."PSAL"),
 '0',
-d.geom
+ST_GeometryFromText(COALESCE('POINT('||avg(d."LONGITUDE")||' '||avg(d."LATITUDE")||')'), '4326') -- geom is re-created from averaged positions over 1min
 FROM auv.auv_trajectory_st_data d, marvl3."500m_isobath" p, marvl3.source s, marvl3."australian_continent" pp
 WHERE ST_CONTAINS(p.geom, d.geom)
 AND ST_CONTAINS(pp.geom, d.geom) = FALSE
 AND s.table_name = 'auv_trajectory_st_data' 
 AND d."TIME" >= '1995-01-01' 
-AND d."TIME" < '2015-01-01';
+AND d."TIME" < '2015-01-01'
+AND EXTRACT(MINUTE FROM date_trunc('minute', d."TIME" AT TIME ZONE 'UTC')) IN (0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55) -- only keep 1 sample every 5min
+GROUP BY s.source_id, d.file_id, date_trunc('minute', d."TIME" AT TIME ZONE 'UTC'); -- at 10knots, 1min <=> ~300m (distance over which averaging is still sensible)
