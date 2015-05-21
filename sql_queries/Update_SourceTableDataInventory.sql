a8c42d248e8879f280949eec935962f475b36770
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-05-21 16:23:55 +1000

    replaced source_id (does not exist in spatial_subset_filtered) by collection_id in script

--- sql_queries/Update_SourceTableDataInventory.sql
+++ sql_queries/Update_SourceTableDataInventory.sql
@@ -3,13 +3,13 @@ SET SEARCH_PATH = marvl3, public;
 \echo 'Data Inventory'
 
 WITH a AS (SELECT 
-m.source_id,
+m.collection_id,
 count(distinct m.origin_id) AS n_deployments,
 count(*) AS n_measurements
 FROM marvl3.spatial_subset_filtered m 
-GROUP BY m.source_id)
+GROUP BY m.collection_id)
 
 UPDATE marvl3.source s
 SET n_deployments = a.n_deployments,
 n_measurements = a.n_measurements 
-FROM a WHERE s.source_id = a.source_id 
+FROM a WHERE s.source_id = a.collection_id
