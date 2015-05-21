b7c9962acf82865474e052d8b6386ec54197a05f
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-05-21 11:54:29 +1000

    Added bathy ga files and new script to create bathy_ga table

---
+++ sql_queries/Create_BathyGATable.sql
@@ -0,0 +1,8 @@
+﻿SET SEARCH_PATH = marvl3, public;
+
+DROP TABLE IF EXISTS bathy_ga;
+CREATE TABLE bathy_ga (
+	"LONGITUDE" double precision,
+	"LATITUDE" double precision,
+	"DEPTH" real
+);
---
+++ sql_queries/Populate_BathyGATable.sql
@@ -0,0 +1,9 @@
+﻿SET SEARCH_PATH = marvl3, public;
+
+-- bathy_ga
+\echo 'bathy_ga'
+COPY bathy_ga ("LONGITUDE", "LATITUDE", "DEPTH") FROM '../bathy/Australia_GA_1kmres_1.xyz' WITH DELIMITER ' '
+COPY bathy_ga ("LONGITUDE", "LATITUDE", "DEPTH") FROM '../bathy/Australia_GA_1kmres_2.xyz' WITH DELIMITER ' '
+COPY bathy_ga ("LONGITUDE", "LATITUDE", "DEPTH") FROM '../bathy/Australia_GA_1kmres_3.xyz' WITH DELIMITER ' '
+COPY bathy_ga ("LONGITUDE", "LATITUDE", "DEPTH") FROM '../bathy/Australia_GA_1kmres_4.xyz' WITH DELIMITER ' '
+COPY bathy_ga ("LONGITUDE", "LATITUDE", "DEPTH") FROM '../bathy/Australia_GA_1kmres_5.xyz' WITH DELIMITER ' '
