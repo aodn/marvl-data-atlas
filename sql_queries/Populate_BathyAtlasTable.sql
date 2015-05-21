94f6a8272ab026fa3a6c7c3f9b72892c190a6cfa
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2015-05-21 12:24:56 +1000

    fixed populate bathy atlas with upper case

--- sql_queries/Populate_BathyAtlasTable.sql
+++ sql_queries/Populate_BathyAtlasTable.sql
@@ -8,8 +8,8 @@ INSERT INTO bathy_atlas (
 "DEPTH"
 )
 select
-(width_bucket(b.longitude, 110.875, 155.125, 177)-1)*0.25+111,
-(width_bucket(b.latitude, -2.875, -45.125, 169)-1)*-0.25-3,
-max(b.depth)
+(width_bucket(b."LONGITUDE", 110.875, 155.125, 177)-1)*0.25+111,
+(width_bucket(b."LATITUDE", -2.875, -45.125, 169)-1)*-0.25-3,
+max(b."DEPTH")
 from marvl3.bathy_ga b
-GROUP BY width_bucket(b.longitude, 110.875, 155.125, 177), width_bucket(b.latitude, -2.875, -45.125, 169);
+GROUP BY width_bucket(b."LONGITUDE", 110.875, 155.125, 177), width_bucket(b."LATITUDE", -2.875, -45.125, 169);
